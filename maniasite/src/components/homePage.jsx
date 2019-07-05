a8a6bc938c2aca5bbb206ed50f56ed1b555584f8
John <huang.john1357@gmail.com>
2019-07-04 23:46:17 -0400

    added starting files

---
+++ maniasite/src/components/homePage.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class HomePage extends Component {
+    //state = {  }
+    render() { 
+        return <button class="btn-success">Play</button>;
+    }
+}
+ 
+export default HomePage;
