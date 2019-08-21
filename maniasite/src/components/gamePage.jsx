c2099d379a82346196d0338eff45e0b4c0dee7cf
John <huang.john1357@gmail.com>
2019-08-20 23:44:49 -0400

    can only count during the count down now

--- maniasite/src/components/gamePage.jsx
+++ maniasite/src/components/gamePage.jsx
@@ -3,9 +3,17 @@ import { Route, Switch, NavLink } from "react-router-dom";
 import Home from "./homePage";
 class Game extends Component {
   state = {
-    z: 0,
-    x: 0
+    count: 0,
+    time: 5,
+    seconds: 5,
+    started: false
   };
+  constructor() {
+    super();
+    this.timer = 0;
+    this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
+  }
   componentDidMount() {
     document.addEventListener("keydown", this.onKeyPressed);
   }
@@ -13,33 +21,49 @@ class Game extends Component {
   componentWillUnmount() {
     document.removeEventListener("keydown", this.onKeyPressed);
   }
-  handleIncrementZ() {
+  handleIncrement = () => {
     console.log("added to Z");
-    //z++;
-  }
-  handleIncrementX() {
-    console.log("added to X");
-    //z++;
-  }
-  handleButtonPressZ = event => {
-    if (event.keyCode === 90) {
-      this.handleIncrementZ();
-      console.log("added to X");
+    if (this.state.seconds > 0) {
+      this.setState({ count: this.state.count + 1 });
     }
+    if (!this.state.started) {
+      this.setState({ started: true }, () => {
+        console.log(this.state.started);
+      });
+      //console.log(this.state.started);
+      this.startTimer();
+      console.log("started method" + this.state.started);
+    }
+    //z++;
   };
-  handleButtonPressX = event => {
+
+  handleButtonPress = event => {
     console.log(event.keyCode + "working");
-    if (event.keyCode === 88) {
-      this.handleIncrementX();
+    if (event.keyCode === 88 || event.keyCode === 90) {
+      this.handleIncrement();
     }
   };
-  handleButtonPress() {
-    console.log("worked");
+  startTimer() {
+    if (this.timer == 0 && this.state.seconds > 0) {
+      this.timer = setInterval(this.countDown, 1000);
+    }
+    console.log("started");
+  }
+  countDown() {
+    console.log(this.state.seconds + " seconds" + this.state.started);
+    this.setState({
+      seconds: this.state.seconds - 1
+    });
+    console.log(this.state.seconds + " seconds2" + this.state.started);
+    if (this.state.seconds == 0) {
+      clearInterval(this.timer);
+      console.log("went here");
+    }
   }
   render() {
     return (
       <div
-        onKeyDown={this.handleButtonPressX}
+        onKeyDown={this.handleButtonPress}
         tabIndex="0"
         style={{ outline: 0 }}
       >
@@ -48,15 +72,21 @@ class Game extends Component {
         </div>
         <div className="row">
           <div className="col-md-12 text-center">
-            <button onClick={this.handleButtonPressX} className="btn btn-info">
-              Z
-            </button>
-            <button
-              onKeyPress={this.handleButtonPress}
-              className="btn btn-info"
-            >
-              X
-            </button>
+            <button className="btn btn-info">Z</button>
+            <button className="btn btn-info">X</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <span className="badge badge-primary"> {this.formatCount()} </span>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <span className="badge badge-primary">
+              {" "}
+              {this.formatSeconds()}{" "}
+            </span>
           </div>
         </div>
         <div className="row">
@@ -74,6 +104,14 @@ class Game extends Component {
         </div>
       </div>
     );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count;
+  }
+  formatSeconds() {
+    const { seconds } = this.state;
+    return seconds;
   }
 }
 
